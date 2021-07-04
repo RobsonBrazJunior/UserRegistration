f5717cc0a9a6b5212ae9fa53523731b1fcd87d22
Robson <robsonbrazjunior@gmail.com>
2021-07-04 18:36:01 -0300

    Add user entity

---
+++ UserRegistration.Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace UserRegistration.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CPF { get; set; }
+
+        public string Email { get; set; }
+    }
+}
